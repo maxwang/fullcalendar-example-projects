c938e13cb323de840d0284cd5e6b9fbd7de180af
Max Wang <mwang@1080agile.com>
2019-05-06 22:51:32 +1000

    remove eventrenderer which could not be changed

--- angular/src/app/tenDaysGrid/TenDaysGrid.ts
+++ angular/src/app/tenDaysGrid/TenDaysGrid.ts
@@ -1,11 +1,23 @@
 import { RenderProps, DayGridProps } from '@fullcalendar/daygrid/DayGrid';
 import { DateComponent } from '@fullcalendar/core';
 import { DayGrid } from '@fullcalendar/daygrid';
+import TenDaysGridEventRenderer from './TenDayGridEventRenderer';
+// import DayGridEventRenderer from '@fullcalendar/daygrid/DayGridEventRenderer';
 
 export default class TenDaysGrid extends DayGrid {
   constructor(context, el, renderProps: RenderProps) {
     super(context, el, renderProps);
+    // this.eventRenderer = new TenDaysGridEventRenderer(this);
+    // let eventRenderer = new TenDaysGridEventRenderer(this);
+    // console.log(this.eventRenderer.isDaySegCollision(null, null))
+    console.log(window);
     console.log('TenDaysGrid');
+this.eventRenderer.isd
+  }
+
+  render(props: DayGridProps) {
+    // this.eventRenderer = fun
+    super.render(props);
   }
 
 }
